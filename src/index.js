1c676cf2dcdbb7e625976bce046ab02c140144a9
jackhowa <jackhowaa@gmail.com>
2018-01-02 19:36:50 -0600

    Refactored by splitting out component from index js into components folder

--- src/index.js
+++ src/index.js
@@ -4,15 +4,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import StorePicker from "./components/StorePicker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
-
-class StorePicker extends React.Component {
-  // similar to es6 classes
-  render() {
-    return <p>Hello</p>;
-  }
-}
 
 ReactDOM.render(<StorePicker />, document.querySelector(".App"));

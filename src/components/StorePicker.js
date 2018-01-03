3cbe56b0082bbe8ea46ea8caa91c925f47f66c0b
jackhowa <jackhowaa@gmail.com>
2018-01-02 20:03:02 -0600

    Write some jsx with self-closing tags

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React from "react";
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>;
+    return (
+      <form className="store-selector">
+        <h2>Enter store yo</h2>
+        <input type="text" placeholder="store name here" required />
+        <button type="submit">Submit!</button>
+      </form>
+    );
   }
 }
 

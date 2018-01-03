0bea5cb816effe4efc56b0d58dee6a8aaf32411d
jackhowa <jackhowaa@gmail.com>
2018-01-02 20:15:47 -0600

    Add css of grey button via import statement

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import StorePicker from "./components/StorePicker";
-
+import "./css/style.css";
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
 

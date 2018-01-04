66cdca95497c0ad038fa19fb8ed0170c6d0ef0cd
jackhowa <jackhowaa@gmail.com>
2018-01-03 18:51:40 -0600

    Split out reusable component of Store Picker into app.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import StorePicker from "./components/StorePicker";
 import "./css/style.css";
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
-
-ReactDOM.render(<StorePicker />, document.querySelector(".App"));
